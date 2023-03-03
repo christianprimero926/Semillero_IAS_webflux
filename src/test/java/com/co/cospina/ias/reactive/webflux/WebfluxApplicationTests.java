c0db12e605d318fe18f2f81fa1eeb11d8f3c96f2
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-03-03 10:46:51 -0500

    Initial commit

---
+++ src/test/java/com/co/cospina/ias/reactive/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.cospina.ias.reactive.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
